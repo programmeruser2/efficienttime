3bce0fe96a349b633337fc9f2640f5302e2e94a5
programmeruser2 <firesoldier999@gmail.com>
2022-06-05 12:35:29 -0400

    finish newGoal(). its not everyday that something works immediately without debugging

--- public/js/goals.js
+++ public/js/goals.js
@@ -4,9 +4,12 @@ const questions = {
 	'numericalgoal': 'What is your goal for the final number?',
 	//'booleangoal': 'What is your goal for what you want to achieve?',
 	//'categorical': 'What do you want as the final state?'
-}
+};
+
 //dropped support for boolean and categorical for now
 async function newGoal() {
+	//its not everyday that something works immediately without debugging
+	let options = {};
 	const { value: name } = await Swal.fire({
 		title: 'What is your goal name?',
 		input: 'text',
@@ -47,16 +50,57 @@ async function newGoal() {
 		},
 		showCancelButton: true
 	});
-	const { value: goal } = await Swal.fire({
+	let { value: goal } = await Swal.fire({
 		title: questions[type],
 		input: 'text',
 		inputLabel: 'The goal',
-		inputValue: 'My Goal',
+		//inputValue: '1',
 		showCancelButton: true,
 		inputValidator: (value) => {
 			if (!value) {
-				return 'Please enter a name!';
+				return 'Please enter a value!';
+			}
+			const n = Number(value);
+			if (!Number.isInteger(n)) {
+				return 'Please enter an integer!';
+			}
+			if (type !== 'numericalgoal' && n <= 0) {
+				return 'Please enter positive integer minutes!';
 			}
 		}
 	});
+	goal = Number(goal);
+	options = {
+		goalName: name,
+		type: type,
+		goalDescription: description
+	};
+	let status;
+	if (type === 'numericalgoal') {
+		status = Number((await Swal.fire({
+			title: 'What is your current status?',
+			input: 'text',
+			inputLabel: 'The status',
+			inputValue: '0',
+			showCancelButton: true,
+			inputValidator: (value) => {
+				if (!value) {
+					return 'Please enter a value!';
+				}
+				const n = Number(value);
+				if (!Number.isInteger(n)) {
+					return 'Please enter an integer!';
+				}
+			}
+		})).value);
+		options.numericalStatus = status;
+	}
+	if (type === 'everyday') {
+		options.dailyGoal = goal;
+	} else if (type === 'totaltime') {
+		options.totalGoal = goal;
+	} else if (type === 'numericalgoal') {
+		options.numericalGoal = goal;
+	}
+	api.goals.new(options);
 }
